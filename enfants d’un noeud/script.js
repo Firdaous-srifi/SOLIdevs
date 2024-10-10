b696957694ab87dc86aefb0d62f7c87c5d2bf45f
Firdaous <srififirdaous@gmail.com>
2024-10-10 09:10:52 +0100

    organise

---
+++ enfants d’un noeud/script.js
@@ -0,0 +1,7 @@
+const myElement = document.getElementById("myElement");
+
+// firstChild will return the first paragraph element
+const firstChild = myElement.firstChild;
+
+// firstElementChild will also return the first paragraph element
+const firstElementChild = myElement.firstElementChild;
